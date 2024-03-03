1ad498c4bed22615f0e188a7720fcc043758dc4d
unknown <bandarupallidavid2004@gmail.com>
2024-03-03 23:24:58 +0530

    Last updated, March 3, 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Components/Header'
+import Header from "./Components/Header";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customRed: '#000000',
+      },
+    },
   },
   plugins: [],
 }
